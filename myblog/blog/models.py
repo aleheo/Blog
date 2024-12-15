a6cb725a2deb2d15574947a9a7d78fb7fee51def
aleheo <st.aleheo@mail.ru>
2024-12-15 04:24:37 +0300

    2 Глава - Продвинутые функциональности

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
-    def get_queryset(self) -> models.QuerySet:
+    def get_queryset(self):
         return super().get_queryset() \
             .filter(status=Post.Status.PUBLISHED)
 
@@ -16,7 +17,8 @@ class Post(models.Model):
 
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User, 
                                on_delete=models.CASCADE,
                                related_name="blog_posts")
@@ -40,4 +42,31 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self) -> str:
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self) -> str:
+        return f'Comment by {self.name} on {self.post}'
     
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'alexey.shorin.02@gmail.com'
+EMAIL_HOST_PASSWORD = 'sfjq ssnc rytc iiqd'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
